daaa5dd94a76365a858e6c69a2e30e52c18fb548
khg9160 <khg059160@gmail.com>
2025-05-16 20:58:45 +0900

    2주차: 일정 삭제 및 수정 기능 추가 (재커밋)

--- termproject.c
+++ termproject.c
@@ -12,7 +12,8 @@ typedef struct {
 
 void addschedule(schedule* list, int* count);
 void print_schedule(schedule* list, int count);
-
+void delete_schedule(schedule* list, int* count);
+void update_schedule(schedule* list, int count);
 
 int main() {
 
@@ -24,6 +25,8 @@ int main() {
 		printf("\n--- 일정 관리 프로그램 ---\n");
 		printf("1. 일정 추가\n");
 		printf("2. 일정 출력\n");
+		printf("3. 일정 삭제\n");
+		printf("4. 일정 수정\n");
 		printf("0. 종료\n");
 		printf("메뉴를 선택하세요: ");
 		scanf("%d", &menu);
@@ -32,6 +35,10 @@ int main() {
 			addschedule(list, &count);
 		else if (menu == 2)
 			print_schedule(list, count);
+		else if (menu == 3)
+			delete_schedule(list, &count);
+		else if (menu == 4)
+			update_schedule(list, count);
 		else if (menu == 0)
 			break;
 		else
@@ -80,4 +87,61 @@ void print_schedule(schedule* list, int count)
 			list[i].date, list[i].title, list[i].priority);
 	}
 
+}
+
+void delete_schedule(schedule* list, int* count)
+{
+	int num;
+	if (*count == 0) {
+		printf("삭제할 일정이 없습니다.\n");
+		return;
+	}
+
+	print_schedule(list, *count);
+	printf("삭제할 일정 번호를 입력하세요: ");
+	scanf("%d", &num);
+
+	if (num < 1 || num > *count) {
+		printf("잘못된 번호입니다.\n");
+		return;
+	}
+
+	// 배열을 앞으로 당기기
+	for (int i = num - 1; i < *count - 1; i++) {
+		list[i] = list[i + 1];
+	}
+	(*count)--;
+
+	printf("일정이 삭제되었습니다.\n");
+}
+
+
+void update_schedule(schedule* list, int count) 
+{
+	int num;
+	if (count == 0) {
+		printf("수정할 일정이 없습니다.\n");
+		return;
+	}
+
+	print_schedule(list, count);
+	printf("수정할 일정 번호를 입력하세요: ");
+	scanf("%d", &num);
+
+	if (num < 1 || num > count) {
+		printf("잘못된 번호입니다.\n");
+		return;
+	}
+
+	int i = num - 1;
+	printf("새 일정 제목: ");
+	scanf(" %[^\n]", list[i].title);
+
+	printf("새 날짜 (YYYY-MM-DD): ");
+	scanf("%s", list[i].date);
+
+	printf("새 우선순위 (1:높음, 2:중간, 3:낮음): ");
+	scanf("%d", &list[i].priority);
+
+	printf("일정이 수정되었습니다.\n");
 }
